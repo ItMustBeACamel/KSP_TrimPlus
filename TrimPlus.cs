c687cd8bd15ed20f560b4c0ea3bbd1043d5d2e71
ItMustBeACamel <mindconductor@googlemail.com>
2015-12-08 11:52:40 +0100

    initial commit

---
+++ TrimPlus.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace TrimPlus
+{
+    // This MUST be a continuous list beginning with 0.
+    public enum BindingName
+    {
+        TRIM_PITCH_UP = 0,
+        TRIM_PITCH_DOWN,
+        TRIM_ROLL_LEFT,
+        TRIM_ROLL_RIGHT,
+        TRIM_YAW_LEFT,
+        TRIM_YAW_RIGHT,
+        RESET_TRIM,
+        RESET_TRIM_PITCH,
+        RESET_TRIM_ROLL,
+        RESET_TRIM_YAW,
+        SET_TRIM,
+        SET_TRIM_PITCH,
+        SET_TRIM_ROLL,
+        SET_TRIM_YAW,
+        COUNT
+    }
+
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class TrimPlus : MonoBehaviour
+    {        
+        public static KeyBinding[] Bindings = new KeyBinding[(int)BindingName.COUNT];
+        
+        float trimPitchRate = 0.1f;
+        float trimRollRate = 0.1f;
+        float trimYawRate = 0.1f;
+        
+        public void Awake()
+        {
+            SetDefaultBindings();
+        }
+        
+        /// <summary>
+        /// Sets default key bindings to all not assigned bindings
+        /// </summary>
+        /// <param name="force">force default for all bindings even if not null</param>
+        public static void SetDefaultBindings(bool force = false)
+        {
+            for (int i = 0; i < Bindings.Length; i++)
+            {
+                KeyBinding binding = Bindings[i];
+
+                if(binding == null || force)
+                {
+                    switch((BindingName)i)
+                    {
+                        case BindingName.TRIM_PITCH_UP:
+                            Bindings[i] = new KeyBinding(KeyCode.PageDown, KeyCode.None);
+                            break;
+                        case BindingName.TRIM_PITCH_DOWN:
+                            Bindings[i] = new KeyBinding(KeyCode.PageUp, KeyCode.None);
+                            break;
+                        default:
+                            Bindings[i] = new KeyBinding(KeyCode.None, KeyCode.None);
+                            break;
+                    }
+
+                }
+            }
+        }
+
+        public static void LoadFromConfig()
+        {
+            try
+            {
+                ConfigNode node;
+                node = ConfigNode.Load("trimPlus.cfg");
+
+                for (int i = 0; i < Bindings.Length; i++)
+                {
+                    if(node == null)
+                    {
+                        Bindings[i] = null;
+                        continue;
+                    }
+                        
+                    KeyBinding newBinding;
+                    try
+                    {
+                        
+                        string nodeName = ((BindingName)i).ToString();                   
+                        newBinding = new KeyBinding();
+                        newBinding.Load(node.GetNode(nodeName));
+                    }
+                    catch(Exception e)
+                    {
+                        Debug.LogException(e);
+                        newBinding = null;
+                    }
+                    Bindings[i] = newBinding;            
+                }
+
+
+            }
+            catch(Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+
+        public static void SaveToConfig()
+        {
+            ConfigNode root = new ConfigNode();
+
+            for (int i = 0; i < Bindings.Length; i++)
+            {
+                ConfigNode newNode = new ConfigNode(((BindingName)i).ToString());
+                Bindings[i].Save(newNode);
+
+                root.AddNode(newNode);
+                
+            }
+
+            root.Save("trimPlus.cfg");
+
+        }
+
+        public void FixedUpdate()
+        {
+            if (Bindings[(int)BindingName.TRIM_PITCH_UP].GetKey())
+            {
+                FlightInputHandler.state.pitchTrim = Mathf.Clamp(FlightInputHandler.state.pitchTrim + trimPitchRate * Time.deltaTime, -1f, 1f);
+                //FlightGlobals.ActiveVessel.ctrlState.pitchTrim = Mathf.Clamp(FlightGlobals.ActiveVessel.ctrlState.pitchTrim + trimPitchRate * Time.deltaTime, -1f, 1f);
+            }
+
+            if (Bindings[(int)BindingName.TRIM_PITCH_DOWN].GetKey())
+            {
+                FlightInputHandler.state.pitchTrim = Mathf.Clamp(FlightInputHandler.state.pitchTrim - trimPitchRate * Time.deltaTime, -1f, 1f);
+                //FlightGlobals.ActiveVessel.ctrlState.pitchTrim = Mathf.Clamp(FlightGlobals.ActiveVessel.ctrlState.pitchTrim - trimPitchRate * Time.deltaTime, -1f, 1f);
+            }
+
+            if (Bindings[(int)BindingName.TRIM_ROLL_LEFT].GetKey())
+            {
+                FlightInputHandler.state.rollTrim = Mathf.Clamp(FlightInputHandler.state.rollTrim - trimRollRate * Time.deltaTime, -1f, 1f);
+            }
+
+            if (Bindings[(int)BindingName.TRIM_ROLL_RIGHT].GetKey())
+            {
+                FlightInputHandler.state.rollTrim = Mathf.Clamp(FlightInputHandler.state.rollTrim + trimRollRate * Time.deltaTime, -1f, 1f);
+            }
+
+            if (Bindings[(int)BindingName.TRIM_YAW_LEFT].GetKey())
+            {
+                FlightInputHandler.state.yawTrim = Mathf.Clamp(FlightInputHandler.state.yawTrim - trimYawRate * Time.deltaTime, -1f, 1f);
+            }
+
+            if (Bindings[(int)BindingName.TRIM_YAW_RIGHT].GetKey())
+            {
+                FlightInputHandler.state.yawTrim = Mathf.Clamp(FlightInputHandler.state.yawTrim + trimYawRate * Time.deltaTime, -1f, 1f);
+            }
+
+            if (Bindings[(int)BindingName.RESET_TRIM_PITCH].GetKey() || Bindings[(int)BindingName.RESET_TRIM].GetKey())
+                FlightInputHandler.state.pitchTrim = 0.0f;
+
+            if (Bindings[(int)BindingName.RESET_TRIM_ROLL].GetKey() || Bindings[(int)BindingName.RESET_TRIM].GetKey())
+                FlightInputHandler.state.rollTrim = 0.0f;
+
+            if (Bindings[(int)BindingName.RESET_TRIM_YAW].GetKey() || Bindings[(int)BindingName.RESET_TRIM].GetKey())
+                FlightInputHandler.state.yawTrim = 0.0f;
+
+            if (Bindings[(int)BindingName.SET_TRIM_PITCH].GetKeyDown() || Bindings[(int)BindingName.SET_TRIM].GetKeyDown())
+                FlightInputHandler.state.pitchTrim = FlightInputHandler.state.pitch;
+
+            if (Bindings[(int)BindingName.SET_TRIM_ROLL].GetKeyDown() || Bindings[(int)BindingName.SET_TRIM].GetKeyDown())
+                FlightInputHandler.state.rollTrim = FlightInputHandler.state.roll;//Mathf.Clamp(/*FlightInputHandler.state.rollTrim +*/ FlightInputHandler.state.roll, -1f, 1f);
+
+            if (Bindings[(int)BindingName.SET_TRIM_YAW].GetKeyDown() || Bindings[(int)BindingName.SET_TRIM].GetKeyDown())
+                FlightInputHandler.state.yawTrim = FlightInputHandler.state.yaw;//Mathf.Clamp(/*FlightInputHandler.state.yawTrim +*/ FlightInputHandler.state.yaw, -1f, 1f);
+
+        }
+
+        
+            
+    }
+}
---
+++ TrimPlusGUI.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace TrimPlus
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    class TrimPlusSettings : MonoBehaviour
+    {
+
+        KeyBinding listeningFor = null;
+        bool listeningForPrimary;
+        
+        static bool likeAVirgin = true;
+
+        public void Awake()
+        {
+            if (likeAVirgin)
+            {
+                TrimPlus.LoadFromConfig();
+                likeAVirgin = false;
+            }
+            TrimPlus.SetDefaultBindings();
+        }        
+
+        public void Update()
+        {
+            if(listeningFor != null)
+            {
+                if(Input.GetKeyDown(KeyCode.Escape))
+                {
+                    listeningFor = null;
+                    return;
+                }
+
+                for (int i = 0; i <= 429; i++)
+                {
+                    try
+                    {
+                        KeyCode code = (KeyCode)i;
+                        if(Input.GetKeyDown(code))
+                        {
+                            if (listeningForPrimary)
+                            {
+                                listeningFor.primary = code;
+                                listeningFor = null;
+                                break;
+                            }
+                            else
+                            {
+                                listeningFor.secondary = code;
+                                listeningFor = null;
+                                break;
+                            }
+                        }
+                    }
+                    catch(Exception)
+                    {
+                        continue;
+                    }
+                    
+                }   
+
+            }
+        }
+
+
+        Rect windowRect = new Rect(0, 0, 600, 50);
+        bool collapsed = true;
+
+        public void OnGUI()
+        {
+            GUI.skin = HighLogic.Skin;
+            windowRect = GUILayout.Window(3453466, windowRect, windowFunc, "TRIM PLUS", GUILayout.Width(600f));
+            //windowRect.height = 0f;
+        }
+
+        public void windowFunc(int id)
+        {
+            GUILayout.BeginArea(new Rect(2f, 2f, 40f, 40f));
+            if (GUI.Button(new Rect(0, 0, 30, 30), collapsed ? "+" : "-"))
+            {
+                collapsed = !collapsed;
+                if (collapsed)
+                    windowRect.height = 42;
+            }
+            GUILayout.EndArea();
+            GUILayout.Space(3);
+            
+            if (!collapsed)
+            {
+                for (int i = 0; i < (int)BindingName.COUNT; i++)
+                {
+                    KeyBinding binding = TrimPlus.Bindings[i];
+
+                    if (listeningFor != null)
+                        GUI.enabled = false;
+                    else
+                        GUI.enabled = true;
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label(((BindingName)i).ToString(), GUILayout.Width(150f));
+
+                    if (GUILayout.Button(listeningFor == binding && listeningForPrimary ? "press key..." : binding.primary.ToString()))
+                    {
+                        listeningFor = binding;
+                        listeningForPrimary = true;
+                    }
+
+                    if (GUILayout.Button(listeningFor == binding && !listeningForPrimary ? "press key..." : binding.secondary.ToString()))
+                    {
+                        listeningFor = binding;
+                        listeningForPrimary = false;
+                    }
+
+                    if (GUILayout.Button("x", GUILayout.ExpandWidth(false)))
+                    {
+                        binding.primary = KeyCode.None;
+                        binding.secondary = KeyCode.None;
+                    }
+
+                    GUILayout.EndHorizontal();
+
+                    GUI.enabled = true;
+
+                }
+
+                if (GUILayout.Button("save"))
+                    TrimPlus.SaveToConfig();
+
+            }
+            GUI.DragWindow();       
+        }
+
+    }
+}
